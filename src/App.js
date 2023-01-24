fd4b82abbc420d18802c59b7952be1a211fe8feb
jaymie <jredman92@gmail.com>
2023-01-23 16:29:45 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CarouselStrap from "./components/CarouselStrap";
 
 function App() {
+  const apiKey = "yYpIWOaoTUEBYsCbi8cCQy76FSqz6FdL";
+  const [homeImages, setHomeImages] = useState([]);
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  async function getImages() {
+    const res = await axios.get(
+      `https://api.giphy.com/v1/gifs/trending?&api_key=${apiKey}&rating=g&limit=5`
+    );
+    setHomeImages(res.data.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Giphy App</h1>
+      <div className="app"></div>
+      <div className="container">
+        <CarouselStrap images={homeImages} />
+      </div>
     </div>
   );
 }
